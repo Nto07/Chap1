6c1c0dd76e8eaa42830c9ea769546b2eab8f5234
Antoine <108286267+Nto07@users.noreply.github.com>
2026-09-21 16:33:30 +0200

    Add files via upload

---
+++ test6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{	
+	int a;
+	printf("Saisir un entier : ");
+	scanf("%d", &a);
+	printf("votre entier est : %d\n", a);
+
+
+	int b;
+	printf("Saisir un entier : ");
+	scanf("%d", &b);
+	int c = b * b;
+	printf("Votre entier est : %d, votre entier au carré est : %d\n", b, c);
+}
